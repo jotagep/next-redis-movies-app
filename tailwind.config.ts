26086d7fcb04078249e6eb23ea7d2fa9e29846b9
jotagep <jotagepweb@gmail.com>
2025-11-25 16:01:21 +0100

    chore: update to node 22

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  purge: [
+  content: [
     'pages/**/*.{js,jsx,ts,tsx}',
     'components/**/*.{js,jsx,ts,tsx}',
     'features/**/*.{js,jsx,ts,tsx}'
